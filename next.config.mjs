623387d3c5725c2fae319b5bceeb1e1cca0da566
mediaawardseo <mediaawardseo@gmail.com>
2024-03-13 08:54:38 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-          port: '',
-        },
-      ],
+        domains: ['cdn.sanity.io'],
     },
-  }
+};
+
 export default nextConfig;
 
-
-
-  
